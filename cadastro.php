6daf9d75de1704c0156fc4ef2fdfb32f2dc6765f
João Campos <joaon.c.jv@gmail.com>
2023-06-14 18:06:01 -0300

    update

---
+++ cadastro.php
@@ -0,0 +1,5 @@
+Parabéns! Seu cadastro foi registrado.
+
+Este projeto consiste apenas na construção de uma tela de login. Após o login, seus dados não vão para nenhum lugar!
+
+Obrigado por testar o site!
